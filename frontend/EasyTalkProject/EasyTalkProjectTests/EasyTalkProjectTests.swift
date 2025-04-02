4c322ce33d637424e14a84f7ba8d3b4a9b1deb09
metropolitStepan <setuparpapr@gmail.com>
2025-04-02 16:03:08 +0300

    Добавил свой проект с SwiftUI + .gitignore обновил для Xcode

---
+++ frontend/EasyTalkProject/EasyTalkProjectTests/EasyTalkProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyTalkProjectTests.swift
+//  EasyTalkProjectTests
+//
+//  Created by Степан Прохоренко on 28.03.2025.
+//
+
+import Testing
+@testable import EasyTalkProject
+
+struct EasyTalkProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
