5d2b223364dce188d90f09783b0e5d453384773d
metropolitStepan <setuparpapr@gmail.com>
2025-04-14 15:30:28 +0300

    Доработан переход по мини-играм

--- frontend/AnimalGuessGameScreen.swift
+++ frontend/AnimalGuessGameScreen.swift
@@ -5,14 +5,15 @@
 //  Created by Степан Прохоренко on 14.04.2025.
 //
 
-//import SwiftUI
-//
-//struct AnimalGuessGameScreen: View {
-//    var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//    }
-//}
-//
-//#Preview {
-//    AnimalGuessGameScreen()
-//}
+import SwiftUI
+
+struct AnimalGuessGameScreen: View {
+    var body: some View {
+        Text("личный кабинет")
+    }
+}
+
+
+#Preview {
+    AnimalGuessGameScreen()
+}
--- frontend/SentenceGameScreen.swift
+++ frontend/SentenceGameScreen.swift
@@ -5,14 +5,15 @@
 //  Created by Степан Прохоренко on 14.04.2025.
 //
 
-//import SwiftUI
-//
-//struct SentenceGameScreen: View {
-//    var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//    }
-//}
-//
-//#Preview {
-//    SentenceGameScreen()
-//}
+import SwiftUI
+
+struct SentenceGameScreen: View {
+    var body: some View {
+        Text("личный кабинет")
+    }
+}
+
+
+#Preview {
+    SentenceGameScreen()
+}
